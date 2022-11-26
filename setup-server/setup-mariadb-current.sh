7edf8fecb983c9c41bfe36a3a5a1a08f54a657e9
Sergei Petrunia <sergey@mariadb.com>
2022-11-26 21:18:04 +0300

    Faster compile

--- setup-server/setup-mariadb-current.sh
+++ setup-server/setup-mariadb-current.sh
@@ -26,7 +26,8 @@ cd mariadb-$BRANCH
 git submodule init
 git submodule update
 cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-  -DWITHOUT_MROONGA:bool=1 -DWITHOUT_TOKUDB:bool=1
+  -DWITHOUT_MROONGA:bool=1 -DWITHOUT_TOKUDB:bool=1 \
+  -DWITH_EMBEDDED_SERVER:bool=0 -DWITH_UNIT_TESTS:bool=0
 make -j8
 cd .. 
 )
