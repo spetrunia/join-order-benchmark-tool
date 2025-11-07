ed162a61cb361e13678ea80fd46f374ab60c275e
Pooja Lamba <pooja.lamba@mariadb.com>
2025-11-07 12:06:24 +0530

    TODO-5660 Check and update join-order-benchmark tool
    Updated server startup script.
    created new scripts to analyse the results.

---
+++ run-mariadb.sh
@@ -0,0 +1,7 @@
+bash setup-server/setup-mariadb-current.sh $1
+bash test-harness/01-setup.sh
+bash test-harness/10-load-data.sh
+bash test-harness/20-generate-workload.sh
+bash test-harness/50-eits-analyze.sh
+bash test-harness/30-run-workload.sh
+bash test-harness/40-save-result.sh
