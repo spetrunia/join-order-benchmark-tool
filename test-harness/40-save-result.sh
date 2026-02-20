599ebb2c3e6376e9fa0d5161b132961d53577b3f
Sergei Petrunia <sergey@mariadb.com>
2026-02-20 16:13:09 +0200

    Save result in .csv.

--- test-harness/40-save-result.sh
+++ test-harness/40-save-result.sh
@@ -3,5 +3,5 @@
 source mysql-vars.sh
 
 VERSION=`$MYSQL_CMD $MYSQL_ARGS -BN -e'select version()'`
-echo "select * from my_job_result" | $MYSQL_CMD $MYSQL_ARGS imdbload | tee -a tmp-mariadb/$VERSION-result.txt
+echo "select * from my_job_result" | $MYSQL_CMD $MYSQL_ARGS imdbload | tee -a tmp-mariadb/$VERSION-result.csv
 
